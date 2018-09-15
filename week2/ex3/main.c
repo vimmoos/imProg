bd317984b87effe43e3108a7b71f84c98116c41a
maxprerealease <j.zuppa@student.rug.nl>
2018-09-15 21:40:42 +0200

    faster but not enough

--- week2/ex3/main.c
+++ week2/ex3/main.c
@@ -16,14 +16,15 @@ int recursion (int a,int n){
     for (int k=1;k<=a/2;k++){
         if(a%k==0){
             // printf("first\n");
-            for (int x=1;x<=k;x++){
-                if((a/k)%x==0 ){
-                    // printf("==========second\n");  
-                    for(int y=1;y<=x;y++){
-                        // printf("====================third\t\t\t%d\t\t\t\t%d\t\t\t\t%d\t\t\t%d\n",k,x,y,k*x*y);
-                        if(x*y*k==a) n++;
-                    }
-                }  
+            for (int x=1;x<=k && x*k>0;x++){
+                if(a/(x*k)<=x && a==x*k*(a/(x*k))) n++;
+                // {
+                //     // printf("==========second\n");  
+                //     for(int y=1;y<=x && x*k*y>0 ;y++){
+                //         // printf("====================third\t\t\t%d\t\t\t\t%d\t\t\t\t%d\t\t\t%d\n",k,x,y,k*x*y);
+                //         if(x*y*k==a) n++;
+                //     }
+                // }  
             }
         }
         
