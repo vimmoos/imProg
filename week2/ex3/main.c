ee4b82675df527b328a84ce05793ead28823b623
maxprerealease <j.zuppa@student.rug.nl>
2018-09-15 02:59:44 +0200

    almost finish week2

--- week2/ex3/main.c
+++ week2/ex3/main.c
@@ -9,33 +9,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int recursion (int a,int b,int c,int n){
-    b++;
-    if((b<a/(b))){
-        if ((a%b==0)) {
-            n++;
-            n = divider(a/b,&c,n)
-        }
-        return a%b==0 ? recursion(a/b,b,c,n) : recursion(a,b,c,n);
+
+int sqroot(int k){
+    int x=1;
+    while (x*x<k) x++;
+    return x*x==k ? x : 0;
+}
+
+int divider (int b,int c, int n, int a){
+    while (c<=b/c && c<=(a/b)/c){
+        if (b%c==0 && (a/b)/c!=b/c && sqroot(b)!=c && sqroot(a)!=b) n++;
+        if ((a/b)%c==0 && (a/b)/c!=b/c && sqroot(b)!=c && sqroot(a)!=b) n++;
+        c++;
     }
+    // for (int x=b;x<=a/x;x++){
+    //     if(a%x==0) {
+    //         c++;
+    //     // if((d/a)%x==0 && (d/a)/x!=a/x) c++;
+    //         if (sqroot(a/x)!=0) c++;
+    //         if (sqroot(x)!=0 && sqroot(x)!=sqroot(a) ) c++;
+ 
+    //     }
+        
+    // }
+    return n;
+}
+
+int recursion (int a,int b,int c,int n){
+
+    
+    for(int x=b;x<=a/x;x++){
+        if ((a%x==0)) {
+            n++;
+            n = divider(x,c,n,a);
+             if (sqroot(a/x)!=0) n++;
+                        
+        }
+    }
+    
     return n;
 }
 
 
-int divider (int a,int *b, int c ){
-    b++;
-    if((*b<a/(*b))){
-        if ((a%*b==0)) c++; 
-        return a%*b==0 ? divider(a/(*b),&b,c) : divider(a,&b,c);
-    }
-    return c; 
-    
-}
-
-
 int main (int argc, char** argv){
-    int k , b=1 , c=1 , n=1;
+    int k , b=2 , c=2 , n=1;
     scanf("%d",&k);
-    printf("%d\n", k==1 ? 1 : recursion(k,b,c,n));
+    int q = recursion(k,b,c,n);
+    printf("%d\n",  q);
     return 0; 
 }
--- week2/ex4/main.c
+++ week2/ex4/main.c
@@ -8,3 +8,39 @@
 */ 
 #include <stdio.h>
 #include <stdlib.h>
+
+ 
+
+int Rnumber (int x){
+    int Rx=0;
+    while (x!= 0){
+        Rx = Rx*10;
+        Rx = Rx+(x%10);
+        x = x/10;
+    }
+    return Rx;
+}
+
+int multipal (int x,int n,int X){
+    for (int k=1;k*k<=X/k;k++){
+        if ( X%k==0 && X/k==x) n=n+1;
+    }
+    return n;
+}
+
+int recursion (int a,int b){
+    int x=a,n=0;
+    while(x<=b){
+        int X = Rnumber(x);
+        n = multipal(x,n,X);
+        x++;
+    }
+    return n;
+}
+
+int main (int argc, char** argv){
+    int a,b;
+    scanf("%d %d",&a,&b);
+    printf("%d\n",recursion(a,b));
+    return 0; 
+}
