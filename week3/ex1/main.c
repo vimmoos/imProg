819dbf703c66fc44ad5e816ae17731afc4e829fa
maxprerealease <j.zuppa@student.rug.nl>
2018-09-17 20:14:40 +0200

    first

--- week3/ex1/main.c
+++ week3/ex1/main.c
@@ -9,6 +9,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include "../../lib/arrLib.h"
 
 int fFunction (int x){
     x=x+1;
@@ -25,14 +26,40 @@ int hFunction (int x){
     return x;
 }
 
-int recursion (int x, char* f){
-    while (sizeof(f)!=0){
-
-    }    
+int recursion(int x , char *c,int i){
+    switch (c[i]){
+        case 'f':
+            x=fFunction(x);
+            i++;
+            x=recursion(x,c,i);
+            break;
+        case 'g':
+            x=gFunction(x);
+            i++;
+            x=recursion(x,c,i);
+            break;
+        case 'h':
+            x=hFunction(x);
+            i++;
+            x=recursion(x,c,i);
+            break;
+        case '=':
+            break;
+        default:
+        printf("default");
+            break;
+    }
+    return x;
 }
+
 int main (int argc, char** argv){
-    char *f=malloc(5*sizeof(char*));
-    scanf("%hhd",f);
-    printf("%hhd\n",f[0]);
+    int x,i=0;
+    scanf("%d ",&x);
+    char *c=charAlloc(10);
+    do{
+        c[i] = getchar();
+        i++;
+    } while( c[i-1] != '=');        
+    printf("%d",recursion(x,c,0));
     return 0; 
 }
