faeedbfb4cb81df00e97e78c4b44dfa6b5b77380
bandoos <luca@bandelli.it>
2018-10-06 05:59:23 +0200

    pecca

--- week4/ex4/main.c
+++ week4/ex4/main.c
@@ -19,6 +19,9 @@ typedef struct Person{
     char *name;
     char *genome;
     int sizeG;
+    int *solutions;
+    int cursorS;
+    int sizeS;
 }Person;
 
 
@@ -31,10 +34,7 @@ typedef struct Database{
 typedef struct Problem{
     Database db;
     char *pattern;
-    int **solutions;
     int sizeP;
-    int cursorS;
-    int sizeS;
 }Problem;
 
 
@@ -45,7 +45,7 @@ void *safeMalloc (int sz);
 
 char *charAlloc (int n);
 
-int **intAlloc (int n);
+int *intAlloc (int n);
 
 Person *PersonAlloc(int sz);
 
@@ -71,6 +71,26 @@ void setSolution(Problem *P,int row,int col);
 
 // define functions body
 
+void freePerson(Person p){
+    free(p.name);
+    free(p.genome);
+    if(p.cursorS != 0) free(p.solutions);
+    return;
+}
+
+void freeDB(Database db){
+    for(int i = 0;i<db.size;i++){
+        freePerson(db.data[i]);
+    }
+    free(db.data);
+    return;
+}
+
+void freeProblem(Problem P){
+    free(P.pattern);
+    return;
+}
+
 void *safeMalloc (int sz){
     void *p= malloc(sz);
     if(p==NULL){
@@ -86,20 +106,28 @@ char *charAlloc (int n){
 }
 
 
-int **intAlloc (int n){
+int *intAlloc (int n){
     return safeMalloc(sizeof(int)*n);
 }
 
-int **matrixIntAlloc(int m,int n){
-    int **c = intAlloc(m);
-    for (int i = 0; i < m; i++ ) c[i]=safeMalloc(n);
-    return c;
-}
+// int **matrixIntAlloc(int m,int n){
+//     int **c = intAlloc(m);
+//     for (int i = 0; i < m; i++ ) c[i]=safeMalloc(n);
+//     return c;
+// }
+
+
+
+
 Problem makeP(char *cp){
     Problem P;
     P.pattern = cp;
     return P;
 }
+
+
+
+
 
 Database makeDB(int maxSZ){
     Database db;
@@ -131,6 +159,9 @@ void parsePerson (char *cp, Database *dbp){
     // translates to parse whatever untill : then : then another string
     char prototype[9] = "%[^:]:%s";
     if (sscanf(cp, prototype , record.name, record.genome) != 2) exit(EXIT_FAILURE);
+    record.sizeG = strlen(record.genome);
+    record.sizeS = 1;
+    record.cursorS = 0;
     addRecordDB(dbp,record);
     return;
 }
@@ -138,8 +169,8 @@ void parsePerson (char *cp, Database *dbp){
 void printDb(Database db){
     printf("[\n");
     for(int i=0;i<db.cursor;i++) 
-        printf("\t{\n\t\t\"name\":\"%s\",\n\t\t\"genome\":\"%s\",\n\t}\n",
-            db.data[i].name,db.data[i].genome);
+        printf("\t{\n\t\t\"name\":\"%s\",\n\t\t\"genome\":\"%s\",\n\t}\n size g %d\t\t size s %d  \n",
+            db.data[i].name,db.data[i].genome,db.data[i].sizeG,db.data[i].sizeS);
     printf("]\n");
     return;
 }
@@ -147,47 +178,32 @@ void printDb(Database db){
 void parseDB(Database *dbp){
     int sz=0;
     scanf("%d",&sz);
+    dbp->size = sz;
+    char buf[11000];
+    char buf1[11000];
     for(int i=0;i<sz;i++){
-        char buf[256];
-        scanf("%s",buf);
-        parsePerson(buf,dbp);
+        scanf("%s%s",buf,buf1);
+        strncat(buf," ",10000);
+        strncat(buf,buf1,10000);
+        parsePerson(buf ,dbp);
     }    
     return;
 }
 
+
+
 void setSolution(Problem *P,int row,int col){
-    if(P->solutions==NULL) P->solutions = matrixIntAlloc(P->db.size,P->sizeS);
-    if(P->cursorS > P->sizeS-1){
-        P->sizeS *= 2;
-        P->solutions[row] = (int *) realloc(P->solutions[row],P->sizeS);
-    } 
-    P->solutions[row][P->cursorS] = col;
-    P->cursorS += 1 ;
-    return;
-}
-
-
-void printProblem(Problem P){
-    printf("\t%s\n",P.pattern);
-    return;
-}
-
-
-void solveProblem(Problem *P){
-    int ncheck;
-    P->cursorS = 0;
-    for(int i=0;i<=P->db.size;i++){
-        for(int j=0;j<P->db.data->sizeG;j++){
-            ncheck = 0;
-            for(int k=0;k<P->sizeP;k++) {
-                if(P->pattern[k] == P->db.data->genome[j+k]) {
-                    ncheck++;
-                    if(ncheck==P->sizeP) setSolution(P,i,j);
-                }
-
-            }
-        }
+    if(P->db.data[row].cursorS == 0){
+        //printf("alloc solutions");
+        P->db.data[row].solutions = intAlloc(P->db.data[row].sizeS);
     }
+    if(P->db.data[row].cursorS > P->db.data[row].sizeS-1){
+        //printf("realloc solutions");
+        P->db.data[row].sizeS *= 2;
+        P->db.data[row].solutions = (int *) realloc(P->db.data[row].solutions,P->db.data[row].sizeS);
+    } 
+    P->db.data[row].solutions[P->db.data[row].cursorS] = col;
+    P->db.data[row].cursorS += 1;
     return;
 }
 
@@ -195,24 +211,61 @@ void solveProblem(Problem *P){
 void setProblem(Problem *P,Database db){
     P->db = db;
     P->db.cursor = 0;
-    P->db.data->sizeG = strlen(P->db.data->genome);
+    //printf("length pattern");
+    //printf("%d\n",strlen(P->pattern));
     P->sizeP = strlen(P->pattern);
-    P->sizeS = 1;
     return;
 }
 
 
+void solveProblem(Problem *P){
+    int ncheck;
+    for(int i=0;i<P->db.size;i++){
+        //printf("\tloop n%d\n",i);
+        for(int j=0; j <= (P->db.data[i].sizeG) - (P->sizeP);j++){
+            if(P->db.data[i].cursorS!=0) break;
+            ncheck = 0;
+            //printf("\t\t\tloop n j %d\n",j);
+            for(int k=0;k<P->sizeP;k++) {
+                //printf("\t\t\t\tloop n k %d\n",k);
+                if(j+k >= P->db.data[i].sizeG) break;
+                if(P->pattern[k] == P->db.data[i].genome[j+k]) {
+                    ncheck++;
+                    if(ncheck==P->sizeP) setSolution(P,i,j);
+                }else break;
+
+            }
+        }
+    }
+    return;
+}
+
+void printSolution(Problem *P){
+    int j=0;
+    for(int i = 0;i<P->db.size;i++){
+        if(P->db.data[i].cursorS != 0){
+                printf("%s: shift=%d\n",P->db.data[i].name,P->db.data[i].solutions[0]);
+                j++;
+        }
+    }
+    printf("%d match%s found.\n",j, j == 1 ? "":"es");
+    return;
+}
+
 void structManager(){
-    char *pattern = charAlloc(256);
+    char *pattern = charAlloc(12048);
     scanf("%s",pattern);
     Problem P = makeP(pattern);
-    // printProblem(P);
-    Database db = makeDB(320);
+    printProblem(P);
+    Database db = makeDB(12048);
     parseDB(&db);
-    // printDb(db);
+    printDb(db);
     setProblem(&P,db);
     solveProblem(&P);
+    //printf("solved\n");
     printSolution(&P);
+    freeDB(db);
+    freeProblem(P);
     return;
 }
 
