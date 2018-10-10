049c1d8d061c00f1007820831c2f3ab6b5af8079
max <j.zuppa@student.rug.nl>
2018-10-10 17:24:21 +0200

    almost finished week5

--- week5/ex4/main.c
+++ week5/ex4/main.c
@@ -10,10 +10,61 @@
 #include <stdlib.h>
 #include <math.h>
 
+typedef struct JumpArr{
+    int *arr;
+    int size;
+    int cursor;
+    int reachable;
+}JumpArr;
 
+void *safeMalloc (int sz){
+    void *p= malloc(sz);
+    if(p==NULL){
+        printf("error cannot alloc %d",sz);
+        exit(EXIT_FAILURE);
+    }
+    return p;
+}
+
+int *intAlloc (int n){
+    return safeMalloc(sizeof(int)*n);
+}
+
+JumpArr makeJump(int sz){
+    JumpArr jA;
+    jA.cursor = sz-1 ;
+    jA.arr = intAlloc(sz);
+    return jA;
+}
+
+JumpArr collectInput(){ 
+    int n;
+    scanf("%d",&n);
+    JumpArr jump = makeJump(n);
+    for(int i=0;i<n;i++){
+        scanf("%d",&jump.arr[i]);
+    }
+    return jump;
+}
+
+int  reachable(JumpArr *jump){
+    if(jump->cursor == 0) return 1;
+    
+    for(int i = jump->cursor-1 ; i >= 0; i--){
+        //printf("cursor == %d\t index == %d\n",jump->cursor,i);
+        if(jump->cursor - i <= jump->arr[i]){
+            //printf("cursor == %d\n",jump->cursor);
+            jump->cursor = i;
+            return reachable(jump);
+        }
+    }
+    return 0 ;
+}
 
 
 int main (int argc, char** argv){
+    JumpArr jump = collectInput();
 
+    printf("%s\n",reachable(&jump) == 1 ? "YES" : "NO" );
     return 0; 
 }
