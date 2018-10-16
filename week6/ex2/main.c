f24f295cac44a8a2a7fcc4eca9c791528ab39551
max <j.zuppa@student.rug.nl>
2018-10-16 17:56:51 +0200

    start last week

---
+++ week6/ex2/main.c
@@ -0,0 +1,16 @@
+/* JSON 
+{
+    'author':'massimilianoFalzari'
+    'studentNumber':'S3459101'
+    'date':'September2018'
+    'gitRepo':'imperativeProgramming' 
+} 
+*/ 
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main (int argc, char** argv){
+    structManager();
+    return 0; 
+}
